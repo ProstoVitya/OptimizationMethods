e9071221751a1cb59ff731da84149d3771744ad4
Виктор Минеев <Prosto_Vitya1337@outlook.com>
2022-03-08 13:50:34 +0400

    Fibbonachi method fix. iterations counter fix.

--- OptimizationMethods/Program.cs
+++ OptimizationMethods/Program.cs
@@ -19,7 +19,7 @@ namespace OptimizationMethods
             int count = 0;
             while (Math.Abs(a - b) > precision)
             {
-                ++count;
+                count+=2;
                 double x = (a + b) / 2;
                 double F1 = f(x - epsilon / 2);
                 double F2 = f(x + epsilon / 2);
@@ -34,7 +34,7 @@ namespace OptimizationMethods
 
         private static double FindMinByGold(double a, double b, Func<double, double> f, double epsilon, double t)
         {
-            int count = 0;
+            int count = 2;
             double f1 = f(Math.Abs(a - b) * 0.382 + a);
             double f2 = f(Math.Abs(a - b) * 0.618 + a);
             while (Math.Abs(a - b) > epsilon)
@@ -59,18 +59,32 @@ namespace OptimizationMethods
 
         private static double FindMinByFibbonachi(double a, double b, Func<double, double> f, double epsilon, double t)
         {
-            int n = 15;
+            int n = 12;
+            double f1 = Fibbonachi(n - 1), f2 = Fibbonachi(n + 1), f3 = Fibbonachi(n);
+            double x1 = a + f1 / f2 * (b - a), x2 = a + f3 / f2 * (b - a);
+            double y1 = f(x1);
+            double y2 = f(x2);
             for (int i = 0; i < n; i++)
-            {
-                double f1 = Fibbonachi(n - i - 1), f2 = Fibbonachi(n - i + 1), f3 = Fibbonachi(n - i);
-                double x1 = a + f1 / f2 * (b - a), x2 = a + f3 / f2 * (b - a);
-                if (f(x1) >= f(x2))
+            {  
+                if (y1 > y2)
+                {
                     a = x1;
+                    x1 = x2;
+                    x2 = b - (x1 - a);
+                    y1 = y2;
+                    y2 = f(x2);
+                }
                 else
+                {
                     b = x2;
+                    x2 = x1;
+                    x1 = a + (b - x2);
+                    y2 = y1;
+                    y1 = f(x1);
+                }
             }
             Console.WriteLine("Interations count:" + n);
-            return (a + b) / 2;
+            return (x1 + x2) / 2;
         }
 
         private static int Fibbonachi(int n)
