0c9b401eb4e8ee87f6ba48cadf521667011301df
ProstoVitya <79058360+ProstoVitya@users.noreply.github.com>
2022-04-25 21:36:56 +0400

    Main fix

--- SecondLab/Program.cs
+++ SecondLab/Program.cs
@@ -12,10 +12,12 @@ namespace SecondLab
             var ans1 = Descent.CoordinateDescent(Functions.Fxy, x, step, epsilon);
             Console.WriteLine($"{ans1[0]} {ans1[1]}\n");
 
-            var ans2 = Descent.GradientDescent(Functions.Fxy, Functions.GradFxy, x, step, epsilon);
+            double[] x2 = { -1d, -2d };
+            var ans2 = Descent.GradientDescent(Functions.Fxy, Functions.GradFxy, x2, step, epsilon);
             Console.WriteLine($"{ans2[0]} {ans2[1]}\n");
 
-            var ans3 = Descent.NewtonDescent(Functions.GradFxy, Functions.Hmatrix, x, epsilon);
+            double[] x3 = { -1d, -2d };
+            var ans3 = Descent.NewtonDescent(Functions.GradFxy, Functions.Hmatrix, x3, epsilon);
             Console.WriteLine($"{ans3[0]} {ans3[1]}\n");
         }
     }
